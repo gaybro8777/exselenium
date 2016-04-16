481f03b9c176f24e53b180e24aa5c8e3cccdfe4c
Nathan Johnson <najohnson@redventures.com>
2016-04-15 21:33:52 -0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Selenium.Mixfile do
 
   def project do
     [app: :selenium,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      description: "Elixir wrapper for selenium webdriver protocol",
